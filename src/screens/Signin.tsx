77022270db0f17a89bdf7152887e224eacee09a6
CaioLuppo <luppocode@outlook.com>
2022-07-18 15:06:20 -0300

    Adicionando primeira tela com código incompleto

---
+++ src/screens/Signin.tsx
@@ -0,0 +1,13 @@
+// Aqui você importa alguns componentes
+
+import { View, Text } from 'react-native';
+
+// Funções ou componentes
+
+export function Signin() {  // export é usado para acessar a função de outros lugares
+    return(
+        <View>
+            <Text>Olá, Caio!</Text>
+        </View>
+    );
+}
