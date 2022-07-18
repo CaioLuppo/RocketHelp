da56ab8192f9a3acfaae15166f230f0b588a9cf1
CaioLuppo <luppocode@outlook.com>
2022-07-18 16:48:53 -0300

    Adicionando react-native-svg-transform para os logos

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -2,12 +2,17 @@
 
 import { VStack, Heading } from 'native-base';
 
+// Assets
+import Logo from '../assets/logo_primary.svg';
+
 // Funções ou componentes (Usar camelCase)
 
 export function SignIn() {
     return (
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
             
+            <Logo />
+
             <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
                 Acesse sua conta
             </Heading>
