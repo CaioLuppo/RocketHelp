c6fe7e23183dcf2abbf2c7eb810565325bd8e858
CaioLuppo <luppocode@outlook.com>
2022-07-19 19:10:08 -0300

    Terminando botão (código)

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -1,4 +1,4 @@
-import { Text, IButtonProps, Button, VStack } from 'native-base';
+import { Text, IButtonProps, Button, VStack, useTheme } from 'native-base';
 
 type Props = IButtonProps & {
     title: string;
@@ -7,11 +7,26 @@ type Props = IButtonProps & {
 };
 
 export function Filter({ title, isActive = false, type, ...rest } : Props) {
+
+  const {colors} = useTheme();
+
+  const colorType = type === 'open' ? colors.secondary[700] : colors.green[300];
+
   return (
+
     <Button
         variant="outline"
+        borderWidth={isActive ? 1 : 0}
+        borderColor={colorType}
+        bgColor='gray.700'
+        flex={1}
+        size="sm"
+        {...rest}
     >
+        
+        <Text color={isActive ? colorType : "gray.300"} fontSize="xs" textTransform="uppercase"> {title} </Text>
 
     </Button>
+    
   );
 }
